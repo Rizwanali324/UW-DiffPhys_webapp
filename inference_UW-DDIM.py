338d654e3c4c766baf066dd8796e965514a7e770
Rizwan Muzammal <129890916+Rizwanali324@users.noreply.github.com>
2025-01-01 13:28:25 +0500

    Update inference_UW-DDIM.py

--- inference_UW-DDIM.py
+++ inference_UW-DDIM.py
@@ -78,50 +78,43 @@ def main():
     torch.backends.cudnn.benchmark = True
 
     diffusion = DenoisingDiffusionUWPhysical(args, config)
-    
-
     diffusion.load_ddm_ckpt(args.resume, ema=True)
-    
     diffusion.model_theta.eval()
     diffusion.model_phi.eval()
-    # breakpoint()
+
     with torch.no_grad():
-        
-        ### eval x_cond folder
-        x_cond_fnames = os.listdir(args.condition_image) #Image.open(args.condition_image)
-        for fname in x_cond_fnames:
-            fname_ = fname.split('.')[0]
-            fname = f"{args.condition_image}/{fname}"
-            # idx = fname.split('/')[-1].split('.bmp')[0]
-            # print(fname)
-            # breakpoint()
-            x_cond = Image.open(fname)
-            x_cond = x_cond.resize((config.data.image_size, config.data.image_size), Image.Resampling.LANCZOS)
-            x_cond = to_tensor(x_cond).to(diffusion.device)
-            # utils.logging.save_image(x_cond, f"results/input.png")
-            x_cond = data_transform(x_cond[None, :, :, :])
-            # print(x_cond.size())
-
-            x = torch.randn(x_cond.size(), device=diffusion.device)
-            t = time.time()
-            y_output, _x0, A, T, y0 = diffusion.sample_image_(x_cond, x, eta=args.eta)
-            # y_output = diffusion.sample_image_(x_cond, x, eta=args.eta)
-            print(f"Total time taken: {time.time() - t}\n")
-            y_output = inverse_data_transform(y_output)
-            #_x0_output = inverse_data_transform(_x0)
-            # A_output = inverse_data_transform(A)
-            # T_output = torch.tensor(np.dot(inverse_data_transform(T).cpu().numpy().transpose(0,2,3,1), [0.2989, 0.5870, 0.1140]))
-            # y0_output = inverse_data_transform(y0)
-            # breakpoint()
-
-            utils.logging.save_image(y_output, f"results/out/uieb-90/128x128/uw-diffphys/{fname_}.png")
-            # utils.logging.save_image(y_output, f"results/out/u45/128x128/uw-diffphys/{fname_}.png")
-            # utils.logging.save_image(y_output, f"results/out/uieb-chal60/128x128/uw-diffphys/{fname_}.png")
-            
-            #utils.logging.save_image(_x0_output, f"results/out/uieb-chal60/128x128/uw-diffphys/{fname_}_x0.png")
-
-            # utils.logging.save_image(_x0_output, f"results/out/suid_uieb/{idx}_x0_.png")
-        
-        # print(f"Total time taken: {time.time() - t}")
+      if os.path.isdir(args.condition_image):
+          # Process directory of images
+          x_cond_fnames = os.listdir(args.condition_image)
+          for fname in x_cond_fnames:
+              # Remove the folder path from the filename
+              fname_ = os.path.splitext(fname)[0]
+              fname = os.path.join(args.condition_image, fname)
+              x_cond = Image.open(fname)
+              x_cond = x_cond.resize((config.data.image_size, config.data.image_size), Image.Resampling.LANCZOS)
+              x_cond = to_tensor(x_cond).to(diffusion.device)
+              x_cond = data_transform(x_cond[None, :, :, :])
+              x = torch.randn(x_cond.size(), device=diffusion.device)
+              t = time.time()
+              y_output, _x0, A, T, y0 = diffusion.sample_image_(x_cond, x, eta=args.eta)
+              print(f"Total time taken: {time.time() - t}\n")
+              y_output = inverse_data_transform(y_output)
+              # Save the image with the result path (ignoring subfolder structure)
+              utils.logging.save_image(y_output, f"results/{fname_}.png")
+      else:
+          # Handle a single image
+          fname = args.condition_image
+          fname_ = os.path.splitext(os.path.basename(fname))[0]  # Extract filename without path
+          x_cond = Image.open(fname)
+          x_cond = x_cond.resize((config.data.image_size, config.data.image_size), Image.Resampling.LANCZOS)
+          x_cond = to_tensor(x_cond).to(diffusion.device)
+          x_cond = data_transform(x_cond[None, :, :, :])
+          x = torch.randn(x_cond.size(), device=diffusion.device)
+          t = time.time()
+          y_output, _x0, A, T, y0 = diffusion.sample_image_(x_cond, x, eta=args.eta)
+          print(f"Total time taken: {time.time() - t}\n")
+          y_output = inverse_data_transform(y_output)
+          # Save the image with the result path (ignoring subfolder structure)
+          utils.logging.save_image(y_output, f"results/{fname_}.png")
 
 main()
